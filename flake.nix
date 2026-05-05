2f49ca0b65082a319cb385e9855ab5febd1ea55b
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-05-05 11:02:03 -0600

    Add initial configuration files for flake support and Java development environment

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Java template: jdk17";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jdk17
+        ];
+
+        shellHook = ''
+        '';
+      };
+    };
+}
